856b5ca9d2d7cded7eb5c616a0c0b4c5e536a679
GerardoRoman <offthegrd3657@gmail.com>
2023-02-15 16:55:34 -0500

    got to list to in display descending order

--- word_frequency.py
+++ word_frequency.py
@@ -11,12 +11,14 @@ def remove_punctuation(words):
     stripped_file = words.translate(str.maketrans('', '', string.punctuation))
     return stripped_file
 
+
 def remove_stop_words(word_list):
     cleaned_list = []
     for word in word_list:
         if word not in STOP_WORDS:
             cleaned_list.append(word)
     return cleaned_list
+
 
 def open_file(file):
     '''Uses `open` to read a text file'''
@@ -27,7 +29,15 @@ def open_file(file):
         stripped_file = remove_punctuation(read_file).lower()
         word_list = stripped_file.split()
         cleaned_list = remove_stop_words(word_list)
-        print(cleaned_list)
+        # print(cleaned_list)
+        return cleaned_list
+
+
+def sort_dictionary(dictionary):
+    sorted_word_count_by_frequency = sorted(
+        dictionary.items(), key=lambda x: x[1], reverse=True)
+    # print(sorted_word_count_by_frequency)
+    return sorted_word_count_by_frequency
 
 
 def print_word_freq(file):
@@ -35,7 +45,14 @@ def print_word_freq(file):
     # use `open` to read a text file 
     words_to_count = open_file(file)
     word_count = {}
-    # loop through the list of words, and updated the dictionary to indicate how many of each we have 
+    for word in words_to_count:
+        if word in word_count.keys():
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    sorted_dictionary = sort_dictionary(word_count)
+    print(sorted_dictionary)
+    return sorted_dictionary
 
 
 if __name__ == "__main__":
